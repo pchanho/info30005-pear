164a8e27b488e5424cd0f65d2e32345f12f6191c
pchanho <61894572+pchanho@users.noreply.github.com>
2020-03-22 02:08:02 +1100

    Delete alert_Park(test).js

--- alert_Park(test).js
+++
@@ -1,9 +0,0 @@
-let f = null;
-let l = null;
-
-function getFormValue(){
-    var firstName = document.getElementById("f").value;
-    var lastName = document.getElementById("l").value;
-
-    alert("My name is " + firstName + " " + lastName);
-}
