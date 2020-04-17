22c500b9b0e7c979f788220ed381cee135019880
glennp1 <61995051+glennp1@users.noreply.github.com>
2020-04-17 16:55:55 +1000

    set up conversation skeleton

---
+++ Pear/routes/conversationRoutes.js
@@ -0,0 +1,37 @@
+/* 
+INFO30005 Group Assignment
+
+Authors: Glenn Deevesh Aiden Gemma Dimitri
+*/
+
+const express = require('express');
+
+// add our router
+const counversationRouter = express.Router();
+
+// handle ???
+
+/*
+
+EXAMPLES
+
+// i.e. get all authors
+authorRouter.get('/', (req, res) => authorController.getAllAuthors(req, res));
+
+// handle the GET request to get an author by using author ID
+authorRouter.get('/:id', (req, res) => authorController.getAuthorByID(req, res));
+
+*/
+
+
+// handle ???
+
+
+// handle ???
+
+
+// require the author controller
+const Controller = require('../Pear/controllers/conversationControllers.js');
+
+// export the router 
+module.exports = conversationRouter;
