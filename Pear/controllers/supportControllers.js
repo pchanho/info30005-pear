7a1b51725492bc6d690663a7c4b30b653509f37a
pchanho <61894572+pchanho@users.noreply.github.com>
2020-04-20 00:17:43 +1000

    Finalise Support Controller

--- Pear/controllers/supportControllers.js
+++ Pear/controllers/supportControllers.js
@@ -1,18 +1,57 @@
 var mongoose = require('mongoose');
-const Supports = mongoose.model('supports');
+var Support = mongoose.model('supports');
 
+//Create
+var createSupport = function(req, res, next){
+    var content ={
+        title:req.body.title,
+        body:req.body.body,
+        image:req.body.image,
+        video:req.body.video
+    };
 
+    var data = new Support(content);
+    data.save();
+
+    res.redirect('/support');
+};
 
 //Read
-const getSupportContents = function(req, res){
-    Supports.find()
+var getSupportContents = function(req, res, next){
+    Support.find()
         .lean()
         .then(function(doc) {
-            res.render('index', {items: doc});
+            res.send(doc);
         });
-    // res.send(supports);
+};
+
+//Update
+var updateSupport = function(req, res, next) {
+    var id = req.body.id;
+
+    Support.findById(id, function(err, doc){
+        if(err){
+            console.error('error, no support content found');
+        }
+        doc.title = req.body.title;
+        doc.body = req.body.body;
+        doc.image = req.body.image;
+        doc.video = req.body.video;
+        doc.save();
+    });
+    res.redirect('/support');
+};
+
+//Delete
+var deleteSupport = function(req, res, next){
+    var id = req.body.id;
+    Support.findByIdAndRemove(id).exec();
+    res.redirect('/support');
 };
 
 module.exports = {
-    getSupportContents
+    getSupportContents,
+    createSupport,
+    updateSupport,
+    deleteSupport
 };
