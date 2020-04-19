dfee97aa146644a26a6e8cb5a4d9d7e86d337a34
pchanho <61894572+pchanho@users.noreply.github.com>
2020-04-19 15:58:54 +1000

    Update supportControllers.js

--- Pear/controllers/supportControllers.js
+++ Pear/controllers/supportControllers.js
@@ -1,10 +1,18 @@
 var mongoose = require('mongoose');
 // var Supports = mongoose.model('supports');
-var supports = require('../models/supportModels');
+// var supports = require('../models/supportModels');
+const Supports = mongoose.model(supports);
+
+
 
 //Read
 const getSupportContents = function(req, res){
-    res.send(supports);
+    Supports.find()
+        .lean()
+        .then(function(doc) {
+            res.render('index', {items: doc});
+        });
+    // res.send(supports);
 };
 
 module.exports = {
