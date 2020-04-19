734a95e9183e887f9e495d19d95c8b992f0b4005
dimitrisad <dsadikin@student.unimelb.edu.au>
2020-04-19 16:07:21 +1000

    adding report files
    model, controllers and route required for the report page (first draft)

---
+++ Pear/models/reportModels.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var reportSchema = new Schema({
+    offendee: {
+        type: String, 
+    	required: true
+    },
+    offendee_id:{
+        type: String,
+        required: true
+    },
+    reason:{
+        type: String, //are there better alternative what can we use for a dropdown list that we provide?
+        required: true
+    }
+}, 
+
+{collection: 'Reports'}); //flagged -- is this needed?
+
+mongoose.model('reports', reportSchema);
---
+++ Pear/routes/reportRoutes.js
@@ -0,0 +1,35 @@
+/* 
+INFO30005 Group Assignment
+
+Authors: Glenn Deevesh Aiden Gemma Dimitri
+*/
+
+const express = require('express');
+
+// add our router
+const reportRoutes = express.Router();
+
+/*
+
+EXAMPLES
+
+// i.e. get all authors
+authorRouter.get('/', (req, res) => authorController.getAllAuthors(req, res));
+
+// handle the GET request to get an author by using author ID
+authorRouter.get('/:id', (req, res) => authorController.getAuthorByID(req, res));
+
+*/
+
+
+// require the report controller
+const reportController = require('../reports/reportControllers.js');
+
+//create
+conversationRoutes.post('/create', reportController.createReport);
+
+//delete
+conversationRoutes.delete('/delete', reportController.deleteReport);
+
+// export the router 
+module.exports = reportRoutes;
