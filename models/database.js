25d146d2364602ff2262d16149d760bfb1e6f9be
gseeley <50686979+gseeley@users.noreply.github.com>
2020-04-22 20:16:58 +1000

    working version

--- models/database.js
+++ models/database.js
@@ -1,17 +1,28 @@
+require('dotenv').config()
 var mongoose = require('mongoose');
 
-const uri = "mongodb+srv://Glenn:123@pear-ox5gs.mongodb.net/test?retryWrites=true&w=majority";
+CONNECTION_STRING = "mongodb+srv://Glenn:<password>@pear-ox5gs.mongodb.net/test?retryWrites=true&w=majority";
 /*need to put username, password*/
 
+MONGO_URL =
+CONNECTION_STRING.replace("<password>",process.env.MONGO_PASSWORD);
+mongoose.connect(MONGO_URL || "mongodb://localhost/info30005", {
+ useNewUrlParser: true,
+ useCreateIndex: true,
+ useUnifiedTopology: true,
+ useFindAndModify: false,
+ dbName: "info30005-pear"
+});
+const db = mongoose.connection;
+db.on("error", err => {
+ console.error(err);
+ process.exit(1);
+});
+db.once("open", async () => {
+ console.log("Mongo connection started on " + db.host + ":" +
+db.port);
+});
 
-mongoose.connect(uri,
-    function(err){
-        if(!err){
-            console.log('Connected to mongo.');
-        }else{
-            console.log('Failed to connect to mongo!', err);
-        }
-    });
 
 // INCLUDE YOUR MODELS HERE
 require('./conversationModels.js');
