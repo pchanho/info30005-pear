23342c0281913ec95a9e79fa2a1f4917b5b1c75e
glennp1 <61995051+glennp1@users.noreply.github.com>
2020-04-24 16:05:24 +1000

    created multiple new message routes and controllers

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -22,17 +22,54 @@ var createMessage = function(req, res, next) {
 };
 
 // read all messages
+var readAllMessages = function(req, res, next) {
+
+};
 
 // read all messages from a particular conversation 
 // (for user history and admin purposes)
+var readSpecificMessages = function(req, res, next) {
+
+};
 
 // read one message
+var readOneMessage = function(req, res, next) {
+
+};
 
 // update message
+var updateMessage = function(req, res, next) {
+    var id = req.body.id;
 
-// delete message
+    Messages.findById(id, function(err, doc) {
+        if (err) {
+            console.error('error, no conversation found');
+        }
+        doc.conversationId = req.body.conversationId;
+        doc.sender = req.body.sender;
+        doc.sentAt = req.body.sentAt;
+        doc.message = req.body.message;
+        doc.mediaAttached = req.body.mediaAttached;
+        doc.media = req.body.media;
+        doc.save();
+    });
+    res.redirect('/');
+};
+
+// delete message by id
+var deleteMessage = function(req, res, next) {
+    var id = req.body.id;
+    Messages.findByIdAndRemove(id).exec();
+    res.redirect('/');
+};
 
 // export functions
-module.exports.createMessage = createMessage;
-
+module.exports = {
+    createMessage,
+    readAllMessages,
+    readSpecificMessages,
+    readOneMessage,
+    updateMessage,
+    deleteMessage
+};
 // ADD OTHER FUNCTIONS
