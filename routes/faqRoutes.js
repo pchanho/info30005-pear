41a8c0f780e720ca063e750ae1518d479c5f7004
pchanho <61894572+pchanho@users.noreply.github.com>
2020-04-25 13:35:40 +1000

    Add FAQ CRUD routes

--- routes/faqRoutes.js
+++ routes/faqRoutes.js
@@ -3,8 +3,18 @@ var faqRoutes = express.Router();
 
 var faqController = require('../controllers/faqControllers');
 
+
+//Create FAQ
+faqRoutes.post('/create', faqController.createFaq);
+
 //Read all FAQs
 faqRoutes.get('/read', faqController.getFaq);
+
+//Update
+faqRoutes.post('/update', faqController.updateFaq);
+
+//Delete
+faqRoutes.post('/delete', faqController.deleteFaq);
 
 //Search contents in FAQ
 faqRoutes.get('/search/:query', faqController.searchFaq);
