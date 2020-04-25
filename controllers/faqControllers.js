518d61d50173d98543953e436419977f89a814d9
pchanho <61894572+pchanho@users.noreply.github.com>
2020-04-25 13:35:18 +1000

    Add Faq CRUD

--- controllers/faqControllers.js
+++ controllers/faqControllers.js
@@ -1,6 +1,21 @@
 var mongoose = require('mongoose');
 var Faq = mongoose.model('faqs');
 
+//Create
+var createFaq = function(req, res, next) {
+    var item = {
+        title:req.body.title,
+        body:req.body.body,
+        updatedAt:req.body.updatedAt
+    };
+
+    var data = new Faq(item);
+    data.save();
+
+    res.redirect('/');
+};
+
+
 //Read all registered FAQs
 var getFaq = function(req, res, next){
     Faq.find()
@@ -9,6 +24,30 @@ var getFaq = function(req, res, next){
         res.send(doc);
     })
 };
+
+//Update
+var updateFaq = function(req, res, next) {
+    var id = req.body.id;
+
+    Faq.findById(id, function(err, doc){
+        if(err){
+            console.error('error, no faq found');
+        }
+        doc.title = req.body.title;
+        doc.body = req.body.body;
+        doc.updatedAt = req.body.updatedAt;
+        doc.save();
+    });
+    res.redirect('/');
+};
+
+//Delete
+var deleteFaq = function(req, res, next){
+    var id = req.body.id;
+    Faq.findByIdAndRemove(id).exec();
+    res.redirect('/');
+};
+
 
 //Search FAQ matching with the user input
 var searchFaq = function(req, res, next) {
@@ -23,6 +62,9 @@ var searchFaq = function(req, res, next) {
 
 //Export the controllers
 module.exports = {
+    createFaq,
     getFaq,
+    updateFaq,
+    deleteFaq,
     searchFaq
 };
