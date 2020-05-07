25fa6a1cdcbc595e988d3e9bf5b49b9064ef87a7
pchanho <61894572+pchanho@users.noreply.github.com>
2020-05-07 14:43:34 +1000

    Create conversationConstants.js

---
+++ info30005-pear-master/constants/conversationConstants.js
@@ -0,0 +1,17 @@
+/*
+INFO30005 Group Assignment - Pear: Conversation Constants
+
+Authors: Glenn Deevesh Chanho Gemma Dimitri
+*/
+
+// define conversation constants
+const NOT_FULL = 0
+const FULL = 1
+const ENDED = 2
+
+// export all constants
+module.exports = {
+	NOT_FULL,
+	FULL,
+	ENDED,
+}
